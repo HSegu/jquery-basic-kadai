d892b1d0284d5c387594f75c00e571dc2a72049f
HSegu <h.segchi@gmail.com>
2023-09-10 19:08:42 +0900

    課題4 提出

--- kadai_004/event.js
+++ kadai_004/event.js
@@ -1,30 +1,10 @@
-/*
-$(function(){    
-    $('window').on({
-        'load': () =>{
-            console.log("loadイベントが発生しました");
-        },
-        'scroll': () =>{
-            console.log("scrollイベントが発生しました");
-        },
-    });
-});
-
-
-
-$(function(){    
-    $(window).on('load', function(){
-        console.log("test");
-    });
-});
-
-*/
-
 $(function(){
+    //HTML読込イベント
     $(document).ready(function(){
         console.log("loadイベントが発生しました");
     });
 
+    //スクロールイベント
     $(window).on('scroll', function(){
         console.log("scrollイベントが発生しました");
     });
