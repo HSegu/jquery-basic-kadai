21a8b992112695c294466a80fefb4bb7ead2c911
HSegu <h.segchi@gmail.com>
2023-09-11 23:16:06 +0900

    課題10 提出

---
+++ kadai_010/summary.js
@@ -0,0 +1,18 @@
+$(function(){
+    //文字色変更
+    $('#change-color').on('click', function(){
+        $('#target').css('color', 'red');
+    });
+    //文字変更
+    $('#change-text').on('click', function(){
+        $('#target').text("Hello!");
+    });
+    //フェードアウト
+    $("#fade-out").on('click', function(){
+        $('#target').fadeOut();
+    });
+    //フェードイン
+    $("#fade-in").on('click', function(){
+        $('#target').fadeIn();
+    });
+});
